85e898295a01c1dd9574c88c231bbce3e83d0bac
haylove <hfayelovelady@gmail.com>
2023-03-02 20:10:49 -0600

    Job Track Started

---
+++ client/src/components/JobCard.js
@@ -0,0 +1,9 @@
+function JobCard() {
+    return (
+        <h3>Company</h3>
+        <p>Job Title</p>
+
+    );
+}
+
+export default JobCard;
---
+++ client/src/components/JobTrackList.js
@@ -0,0 +1,17 @@
+import { useDrag } from 'react-dnd'
+
+function JobTrackList() {
+    return (
+        <h1>Job Track List</h1>
+        // <Card sx={{ maxWidth: 345 }}>
+        // <CardMedia
+        //   sx={{ height: 140 }}
+        //   image="/static/images/cards/contemplative-reptile.jpg"
+        //   title="green iguana"
+        // />
+        // </Card>
+    );
+}
+
+export default JobTrackList;
+
