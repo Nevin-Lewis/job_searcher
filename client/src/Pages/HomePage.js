ac9f172c3662170e70fc995c85a18a4635ae3b6e
Nevin Lewis <lewisnevin@utexas.edu>
2023-03-07 21:19:04 -0600

    login

--- client/src/Pages/HomePage.js
+++ client/src/Pages/HomePage.js
@@ -1,17 +1,21 @@
 import LoginForm from "../components/LoginForm";
 import Signup from "../components/SignUpForm";
 import React from "react";
+import Auth from "../utils/auth";
+import { Button } from "react-bootstrap";
 
 function HomePage() {
     return (
         <div>
             <h1>Homepage</h1>
-
+        {Auth.loggedIn() ? ( <Button onClick={Auth.logout}>Logout</Button> ) : (
+            <div>
             <Signup />
 
             <h1> Split</h1>
             <LoginForm />
-
+            </div>
+        )}
         </div>
 
     );
