4ceb04da301a9a82c9bec8d628fb30d34b5d3bdf
Marcie Lucke <marcielucke@Marcies-MBP.attlocal.net>
2023-03-09 16:52:27 -0600

    space between login signup

--- client/src/Pages/HomePage.js
+++ client/src/Pages/HomePage.js
@@ -10,28 +10,32 @@ function HomePage() {
     const navigate = useNavigate();
 
     useEffect(() => {
-        if(Auth.loggedIn()) {
+        if (Auth.loggedIn()) {
             console.log("User Authorized");
             navigate('/Profile');
         }
     }, []);
 
-   // if(Auth.loggedIn() ? navigate('/Profile') : ( )
+    // if(Auth.loggedIn() ? navigate('/Profile') : ( )
     return (
         <div>
-        {Auth.loggedIn() ? ( <Button onClick={Auth.logout}>Logout</Button> ) : (
-        // {Auth.loggedIn() ? navigate('/Profile') : (
-            <div className="logsign">
-                <div className="container" styles={{margin:"10px"}}>
-                <h1>Sign Up</h1>
-                <Signup />
+            {Auth.loggedIn() ? (<Button onClick={Auth.logout}>Logout</Button>) : (
+                // {Auth.loggedIn() ? navigate('/Profile') : (
+                <div className="logsign">
+                    <div className="container" styles={{ margin: "10px" }}>
+
+                        <h1>Sign Up</h1>
+                        <Signup />
+
+                    </div>
+                    <div className="container" styles={{ margin: "10px" }}>
+
+                        <h1>Log In</h1>
+                        <LoginForm />
+
+                    </div>
                 </div>
-                <div className="container" styles={{margin:"10px"}}>
-                <h1>Log In</h1>
-                <LoginForm />
-                </div>
-            </div>
-        )}
+            )}
         </div>
 
     );
